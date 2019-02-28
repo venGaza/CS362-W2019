f022fee8bc3157e071160202a5721d8e0ee66178
Dustin Jones <gazaven@gmail.com>
2019-02-28 00:22:45 -0600

    Created random test generator for smithy and adventurer cards

--- projects/jonesdus/dominion/randomtestadventure.c
+++ projects/jonesdus/dominion/randomtestadventure.c
@@ -1,6 +1,9 @@
 #include "dominion.h"
 #include "dominion_helpers.h"
+#include "interface.h"
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 #include <assert.h>
 #include "rngs.h"
@@ -41,12 +44,78 @@
  * card effect.
  */
 
-void randomTestGenerator () {
+void randomTestAdventure (struct gameState* G) {
+    int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+    int numPlayers;
+    int seed;
+
+    // Random number of players
+    numPlayers = (rand() % 3) + 2;
+
+    // Random seed
+    seed = (rand() % 100) + 1;
+
+    //Initialize base game
+    initializeGame(numPlayers, k, seed, G);
     
+    // Random Turn
+    G->whoseTurn = rand() % numPlayers;
+    
+    // Random deck count *Causes trap error:6
+    //G->deckCount[G->whoseTurn] = rand() % 8;
+
 }
 
 int main () {
+    srand(time(NULL));
+    int i = 0;
+    int testNum = 1;
+    int choice1 = 0;
+    int choice2 = 0;
+    int choice3 = 0;
+    int handPos = 0;
+    int bonus = 0;
+    int handCoinsBefore = 0;
+    int handCoinsAfter = 0;
+    int numHandCardsBefore = 0;
+    int numHandCardsAfter = 0;
 
+    // Initialize game with 4 players, 10 cards, seed of 5, and named G
+    struct gameState G;
+    memset (&G, '\0' , sizeof(struct gameState));
+    
+    for (i = 0; i < 100; i++) {
+        // Randomize the initial gamestate
+        randomTestAdventure(&G);
 
+        // Check coins and number of cards for current player before adventurer is played
+        handCoinsBefore = countHandCoins(G.whoseTurn, &G);
+        numHandCardsBefore = numHandCards(&G);
+
+        // Run the adventurer card on the gamestate
+        cardEffect(adventurer, choice1, choice2, choice3, &G, handPos, &bonus);
+
+        // Check count after adventurer was played
+        handCoinsAfter = countHandCoins(G.whoseTurn, &G);
+        numHandCardsAfter = numHandCards(&G);
+
+        // Print results
+        if (handCoinsAfter >= (handCoinsBefore + 2)) {
+            printf("Test %d: Coin Count Passed ", (testNum));
+        } else {
+            printf("Test %d: Coin Count Failed", (testNum));
+        }
+
+        if (numHandCardsAfter >= (numHandCardsBefore + 2)) {
+            printf("Test %d: Card Count Passed\n", (testNum));
+        } else {
+            printf("Test %d: Card Count Failed\n", (testNum));
+        }
+
+        // Reset for next round
+        testNum++;
+        memset (&G, '\0' , sizeof(struct gameState));
+    }
+    
     return 0;
 }
--- projects/jonesdus/dominion/randomtestcard1.c
+++ projects/jonesdus/dominion/randomtestcard1.c
@@ -2,6 +2,8 @@
 #include "dominion_helpers.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <assert.h>
 #include "rngs.h"
 
@@ -22,21 +24,78 @@
  * --------------------------------------------
  * This function allows player to draw 3 cards from deck and then is discarded.  
  * 
- * The following unit test will check for the following:
- * - the smithy card draws the right number of cards
- * - the smithy card discards to the correct pile
- * - the smithy card draws from the correct pile
- * - the smithy card discarded itself and not another card
- * - assumes initializeGame() and drawCard have been tested and are working as intended
- * - also assumes the discardCard within Smithy is functioning as intended
+ * The following unit test uses a random test generator to create a random game state to test the
+ * functionality of the smithy card effect. 
  */
 
-void randomTestGenerator () {
+void randomTestSmithy (struct gameState* G) {
+    int k[10] = {smithy, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+    int numPlayers;
+    int seed;
+
+    // Random number of players
+    numPlayers = (rand() % 3) + 2;
+
+    // Random seed
+    seed = (rand() % 100) + 1;
+
+    //Initialize base game
+    initializeGame(numPlayers, k, seed, G);
     
+    // Random Turn
+    G->whoseTurn = rand() % numPlayers;
 }
 
 int main () {
+    srand(time(NULL));
+    int i = 0;
+    int testNum = 1;
+    int choice1 = 0;
+    int choice2 = 0;
+    int choice3 = 0;
+    int handPos = 0;
+    int bonus = 0;
+    int discardCountBefore = 0;
+    int discardCountAfter = 0;
+    int numHandCardsBefore = 0;
+    int numHandCardsAfter = 0;
 
+    // Initialize game with 4 players, 10 cards, seed of 5, and named G
+    struct gameState G;
+    memset (&G, '\0' , sizeof(struct gameState));
+    
+    for (i = 0; i < 100; i++) {
+        // Randomize the initial gamestate
+        randomTestSmithy(&G);
+
+        // Check number of cards for current player before adventurer is played
+        discardCountBefore = G.discardCount[G.whoseTurn];
+        numHandCardsBefore = numHandCards(&G);
+
+        // Run the adventurer card on the gamestate
+        cardEffect(smithy, choice1, choice2, choice3, &G, handPos, &bonus);
+
+        // Check count after adventurer was played
+        discardCountAfter = G.discardCount[G.whoseTurn];
+        numHandCardsAfter = numHandCards(&G);
+
+        // Print results
+        if (numHandCardsAfter == (numHandCardsBefore + 1)) {
+            printf("Test %d: Card Count Passed ", (testNum));
+        } else {
+            printf("Test %d: Card Count Failed ", (testNum));
+        }
+
+        if (discardCountAfter == (discardCountBefore + 1)) {
+            printf("Test %d: Discard Count Passed\n", (testNum));
+        } else {
+            printf("Test %d: Discard Count Failed\n", (testNum));
+        }
+
+        // Reset for next round
+        testNum++;
+        memset (&G, '\0' , sizeof(struct gameState));
+    }
 
     return 0;
 }
